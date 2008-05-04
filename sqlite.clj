d7bf8fe8e08912e8f74dfe8363187899ec648152
Eric Rochester <eric@enki.(none)>
2008-05-04 17:24:31 -0400

    refactored the example of using sqlite in clojure
    committer: Eric Rochester <eric@enki.(none)>
    
    
    committer: Eric Rochester <eric@enki.(none)>
    
    --HG--
    extra : convert_revision : b25797245caa547b84a646f129e31e509093af40

--- sqlite.clj
+++ sqlite.clj
@@ -4,38 +4,50 @@
 ;;;; how to call SQLite from Clojure.
 
 (import '(java.sql DriverManager))
+(import '(java.lang Double Integer))
 
 (. Class forName "org.sqlite.JDBC")
 
-(def cxn (. DriverManager getConnection "jdbc:sqlite:test.db"))
-(def stmt (. cxn createStatement))
+(defmulti set-sql-param (fn [stmt n value] (. value getClass)))
 
-(. stmt executeUpdate "CREATE TABLE people (name, occupation);")
+(defmethod set-sql-param :default [stmt n value]
+  (. stmt setString n (str value)))
 
-(def prep (. cxn prepareStatement "INSERT INTO people VALUES (?, ?);"))
-(doto prep
-  (setString 1 "Gandhi")
-  (setString 2 "politics")
-  (addBatch)
-  (setString 1 "Turing")
-  (setString 2 "computers")
-  (addBatch)
-  (setString 1 "Wittgenstein")
-  (setString 2 "smartypants")
-  (addBatch))
+(defmethod set-sql-param Double [stmt n value]
+  (. stmt setDouble n value))
 
-(. cxn setAutoCommit false)
-(. prep executeBatch)
-(. cxn setAutoCommit true)
+(defmethod set-sql-param Integer [stmt n value]
+  (. stmt setInteger n value))
+
+(defn do-batch [stmt & data]
+  (let [
+        add-row (fn add-row [n row]
+                  (if row
+                    (do
+                      (set-sql-param stmt n (first row))
+                      (recur (inc n) (rest row)))
+                    (. stmt addBatch)))
+        ]
+    (loop [data data]
+      (if data
+        (do
+          (add-row 1 (seq (first data)))
+          (recur (rest data)))
+        (. stmt executeBatch)))))
 
 (defn dump-people [rs]
-  (if (. rs next)
-    (do
-      (println "name =" (. rs getString "name"))
-      (println "job  =" (. rs getString "occupation"))
-      (recur rs))
-    (. rs close)))
+  (when (. rs next)
+    (println "name =" (. rs getString "name"))
+    (println "job  =" (. rs getString "occupation"))
+    (recur rs)))
 
-(dump-people (. stmt executeQuery "SELECT * FROM people;"))
-(. cxn close)
+(with-open cxn (. DriverManager getConnection "jdbc:sqlite:test.db")
+  (let [stmt (. cxn createStatement)]
+    (. stmt executeUpdate "CREATE TABLE people (name, occupation);")
+    (. cxn setAutoCommit false)
+    (do-batch (. cxn prepareStatement "INSERT INTO people VALUES (?, ?);")
+              ["Gandhi" "politics"] ["Turing" "computers"] ["Wittgenstein" "smartypants"])
+    (. cxn setAutoCommit true)
+    (with-open rs (. stmt executeQuery "SELECT * FROM people;")
+      (dump-people rs))))
 
